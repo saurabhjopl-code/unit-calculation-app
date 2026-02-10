4a558926792d24f78e131d25c7ec83ac3d111fab
SAURABH MONDAL <saurabh.jopl@gmail.com>
2026-02-10 14:08:09 +0530

    Create device.service.js

---
+++ js/services/device.service.js
@@ -0,0 +1,12 @@
+const DEVICE_KEY = "unit_app_device_id";
+
+export function getDeviceId() {
+  let id = localStorage.getItem(DEVICE_KEY);
+
+  if (!id) {
+    id = "DEV-" + crypto.randomUUID();
+    localStorage.setItem(DEVICE_KEY, id);
+  }
+
+  return id;
+}
