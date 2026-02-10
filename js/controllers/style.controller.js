701104a69478710c22a28716ef51d419fd2ad7d4
SAURABH MONDAL <saurabh.jopl@gmail.com>
2026-02-10 12:14:44 +0530

    Update style.controller.js

--- js/controllers/style.controller.js
+++ js/controllers/style.controller.js
@@ -1,12 +1,24 @@
 import { appState, STEPS } from "../state/app.state.js";
 import { goToStep } from "./flow.controller.js";
 
-export function selectStyle(styleId, imageUrl) {
+/**
+ * Called when user selects a style from search
+ */
+export function selectStyle(styleId) {
+  const style = appState.stylesMap[styleId];
+  if (!style) return;
+
   appState.current.styleId = styleId;
-  appState.current.imageUrl = imageUrl || "./data/No-image.png";
+  appState.current.imageUrl = style.image.url;
+  appState.current.sizes = [];
+  appState.current.units = null;
+
   goToStep(STEPS.STYLE_VERIFIED);
 }
 
+/**
+ * Called when user clicks Verify Style
+ */
 export function verifyStyle() {
   goToStep(STEPS.SIZE_SELECTED);
 }
